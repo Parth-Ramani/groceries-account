9572d5ca59b1f3ca1977cd7c4f56ed58994de3b4
Parth-Ramani <parthramani93@gmail.com>
2022-02-16 15:45:22 +0530

    create the ContactList Component nd adding the btn

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import InputFields from "./Components/InputFields";
 import "./App.css";
 import { useState } from "react";
 import Modal from "./Components/Modal";
-
+import ContactList from "./Components/ContactList";
 function App() {
   const [openForm, setForm] = useState(false);
   return (
@@ -21,6 +21,7 @@ function App() {
       </button>
       <br />
       <button className="accountList">Account List</button>
+      <ContactList />
       {openForm && <Modal setForm={setForm} />}
       <InputFields />
     </div>
---
+++ src/Components/ContactList.js
@@ -0,0 +1,19 @@
+import "./ContactList.css";
+const ContactList = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <button>jj</button>
+        </li>
+        <li>
+          <button>jj</button>
+        </li>
+        <li>
+          <button>jj</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default ContactList;
