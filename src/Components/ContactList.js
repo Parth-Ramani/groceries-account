3ce64721225042f0aedf2b041681e349ec9f6341
Parth-Ramani <parthramani93@gmail.com>
2022-03-04 12:51:09 +0530

    customerlist added inputs and css

--- src/Components/ContactList.js
+++ src/Components/ContactList.js
@@ -6,11 +6,47 @@ const ContactList = (props) => {
         <div>
           <input
             className="search"
-            type="text"
+            type="search"
             placeholder="Search.."
             name="search"
           />
-          <button>Filter</button>
+          <button className="filter">Filter </button>
+        </div>
+        <div className="input__Box">
+          <form>
+            <label className="fname" htmlFor="fname">
+              Full Name
+            </label>
+            <input
+              id="fname"
+              type="text"
+              className="fname_input"
+              required="required"
+              placeholder="Full Name"
+            />
+            {/* <label className="number" for="number">
+              Number
+            </label>
+            <input
+              id="number"
+              type="number"
+              className="number_input"
+              required="required"
+              placeholder="Number"
+            />
+            <br />
+            <label className="address" for="address">
+              Address
+            </label>
+            <input
+              id="address"
+              type="text"
+              className="address_input"
+              required="required"
+              placeholder="Address"
+            />
+            <br /> */}
+          </form>
         </div>
       </div>
     </div>
